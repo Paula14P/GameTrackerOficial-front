694caaa298a9b09401202f1f4733ddc05452971d
Paula14P <paulaphilip31212@gmail.com>
2025-11-04 18:58:39 -1200

    actualización

---
+++ src/components/FormularioJuego.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FormularioJuego() {
+  return (
+    <div>
+      <p>Formulario para agregar juego...</p>
+    </div>
+  );
+}
+
+export default FormularioJuego;

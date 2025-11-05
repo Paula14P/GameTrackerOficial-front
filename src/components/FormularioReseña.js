4c4eb2c723e464264cd8f33c1198e3c8e9d73fa5
Paula14P <paulaphilip31212@gmail.com>
2025-11-04 19:29:24 -1200

    actualización

---
+++ src/components/FormularioReseña.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FormularioReseña() {
+  return (
+    <div>
+      <p>Formulario para agregar reseña...</p>
+    </div>
+  );
+}
+
+export default FormularioReseña;
